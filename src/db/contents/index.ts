842e93857b2ed11ebf6dfe00a70c806d7346464a
Dimas Leopoldo Mérida Flores <48163017+leomerida15@users.noreply.github.com>
2021-08-27 00:43:21 -0400

    auth ok

--- src/db/contents/index.ts
+++ src/db/contents/index.ts
@@ -1,6 +1,8 @@
 import ident_type from './ident_type';
 import roles from './roles';
+import phone from './phone';
 export default async () => {
 	await ident_type();
 	await roles();
+	// await phone();
 };
--- src/db/models/fm_phone.ts
+++ src/db/models/fm_phone.ts
@@ -4,10 +4,10 @@ import fm_Client from './fm_client';
 @Entity()
 export default class fm_phone {
 	@PrimaryGeneratedColumn()
-	id!: number;
+	id?: number;
 
 	@ManyToOne(() => fm_Client, (fm_Client) => fm_Client.phones)
-	@JoinColumn()
+	@JoinColumn({ name: 'id_client', referencedColumnName: 'id' })
 	id_client!: number;
 
 	@Column({ unique: false })
