9c9e370871ca588aceec0aee8342ae125ab29a4b
Dimas L. Merida Flores <dmerida@tranred.com.ve>
2021-09-21 11:04:21 -0400

    creacion de dir_pos

--- src/router/fm/fm.routes.ts
+++ src/router/fm/fm.routes.ts
@@ -15,11 +15,8 @@ FM.route('/FM/client').post(fm_valid_client);
 //
 FM.route('/FM/:id/commerce').post(fm_create_commerce);
 //
-<<<<<<< HEAD
 FM.route('/FM').post(FM_create);
 //
-=======
->>>>>>> bb5d658a0c117c6aac267125e0c31f1f5cf90e61
 FM.route('/FM/client/valid').post(validExistingClient, valid_existin_client);
 
 export default FM;
